2443ef63b5318955a1c12896ca33cc3ac99aeb53
moritz löser <moritz.loeser@gmail.com>
2014-03-27 16:25:45 +0100

    deleted single files from root folder

--- requirement_1.sh
+++
@@ -1,4 +0,0 @@
-echo "R1: the CEP Engine delivers POSTPAID throttle 100 events with various metrics."
-echo "EDRs which have a local QuotaName and TriggerType 1 to 6 are of interest."
-echo "Throttle 100 events have the QuotaStatus=6 and QuotaValue=1."
-echo " "
