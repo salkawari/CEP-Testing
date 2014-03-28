973dce7b92ce5ef306e33ceef5e9bbca1c083f69
sal <sal@sal-HP-Compaq-dc7900-Convertible-Minitower.(none)>
2014-03-28 13:37:29 +0100

    main change - latest config in camel xml and new test case 20 added

---
+++ sals_tests/requirement_20.sh
@@ -0,0 +1,4 @@
+echo "R20: When the data for the primary columns is partially / fully empty, "
+echo "they get redirected to the bad_events subfolder (for possible later analysis)."
+echo "The other records in the EDR stream should still be processed by the CEP engine."
+echo " "
