f4157738ca314026e5215875b03978fbbc7f8823
sal <sal@sal-HP-Compaq-dc7900-Convertible-Minitower.(none)>
2014-04-08 16:16:54 +0200

    removed bug in adapter stop script

--- sals_tests/start_stop_dir/STOP_CEP_ADAPTER.sh
+++ sals_tests/start_stop_dir/STOP_CEP_ADAPTER.sh
@@ -2,7 +2,9 @@
 
 echo "calling $0 $1 $2"
 
-already_running_process=$(ps aux|grep -i jar| grep -v grep | awk '{print $2}')
+cep_adapter_file=$(cat START_STOP_CONFIG.txt|grep -i "cep_adapter_file"|cut -d'=' -f2)
+
+already_running_process=$(ps aux|grep -i "$cep_adapter_file"| grep -v grep | awk '{print $2}')
 if [ $(echo $already_running_process|wc -w) -eq 0 ]
 then
   echo "WARNING!!! The CEP adapter is not running anyway!!!"
@@ -13,10 +15,10 @@ fi
 echo "killing ${already_running_process}.."
 kill $already_running_process
 
-while [ $(ps aux|grep -i jar| grep -v grep | awk '{print $2}'|wc -w) -ne 0 ]
+while [ $(ps aux|grep -i "$cep_adapter_file"| grep -v grep | awk '{print $2}'|wc -w) -ne 0 ]
 do
   echo "waiting on the following processes to stop.."
-  echo $(ps aux|grep -i jar| grep -v grep | awk '{print $2}')
+  echo $(ps aux|grep -i "$cep_adapter_file"| grep -v grep | awk '{print $2}')
   sleep 3
 done
 
